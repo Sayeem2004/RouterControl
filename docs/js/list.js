0d220bfd2778ae1d307b777f70aeadb7bd9d26af
msk295 <msk295@cornell.edu>
2022-10-16 14:34:46 -0400

    temp commit

--- docs/js/list.js
+++ docs/js/list.js
@@ -27,7 +27,7 @@ function updateList() {
 }
 
 function getList() {
-    fetch("/home/jemz/Desktop/RouterControl/data/list.json")
+    fetch("../../data/list.json")
         .then(response => response.json())
         .then(data => {
             list = data;
