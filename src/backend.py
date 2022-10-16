ac93964bcad87c3a8be4effbaab714642eeb79ed
jeremy <jku-benjet20@stuy.edu>
2022-10-16 01:56:11 -0400

    get list of devices

--- src/backend.py
+++ src/backend.py
@@ -1,1 +1,17 @@
-# This is where your backend should go Jeremy.
+from subprocess import check_output
+
+GRAPH_FILENAME = "graph.json"
+LIST_FILENAME = "list.json"
+INFO_FILENAME = "info.json"
+
+def get_connected_devices():
+    devices = check_output(["arp", "-a"])
+    devices = devices.splitlines()
+    devices = [d.split()[0:2] for d in devices]
+    return devices
+
+def main():
+    print(get_connected_devices())
+
+if __name__ == "__main__":
+    main()
